4eecd27267f33eb032b235774609eaa63160e0f3
bishal <bishalbhaandari62@gmail.com>
2024-01-24 18:18:14 +0100

    -get image -process -return json value of contours array

--- MultiObjectOutline.py
+++ MultiObjectOutline.py
@@ -1,30 +1,42 @@
-import json
+import base64
+from flask import Flask, request, jsonify
 import cv2
 import numpy as np
+from flask_cors import CORS
 
-img = cv2.imread('Image/Multiple_Object_Gradient.png')  # read image
-bnw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert img to BnW
-gus_blr = cv2.GaussianBlur(bnw, (5, 5), 0)  # decreasing the noise
-edges = cv2.Canny(gus_blr, 50, 150)  # edge detection
+app = Flask(__name__)
+CORS(app)
 
-contour_img, hire_rel = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # finding the contour
 
-if contour_img:  # validation for contour
-    all_contour_points = []
-    for contour in contour_img:
-        contour_points = contour.reshape(-1, 2)  # representing into 2D array
-        all_contour_points.append(contour_points.tolist())  # rows to list added
+@app.route('/detect_contours', methods=['POST'])
+def detect_contours():
+    try:
+        # Get the image file from the request
+        image_file = request.files['image']
+        img_fs = image_file.read()  # FS to readable form
+        np_ary = np.frombuffer(img_fs, np.uint8)  # binary to ary form
+        image = cv2.imdecode(np_ary, cv2.IMREAD_COLOR)  # to color readable form
+        bnw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gus_blr = cv2.GaussianBlur(bnw, (5, 5), 0)
+        edges = cv2.Canny(gus_blr, 50, 150)
+        contour_img, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contour_img:
+            all_contour_points = [contour.reshape(-1, 2).tolist() for contour in contour_img]
+            contour_dict = {i + 1: sublist for i, sublist in enumerate(all_contour_points)}
 
-    contour_dict = {i + 1: sublist for i, sublist in enumerate(all_contour_points)}
+            cv2.drawContours(image, contour_img, -1, (0, 255, 0), 3)  # drawing the outline
 
-    with open('contoursFile.json', 'w') as jf:
-        for key, value in contour_dict.items():
-            jf.write(json.dumps({key: value}) + '\n')  # save as json file
+            _, buffer = cv2.imencode('.png', image)  # parsing to base64 file
+            image_data = base64.b64encode(buffer).decode('utf-8')  # img data
 
-    cv2.drawContours(img, contour_img, -1, (0, 255, 0), 1)  # drawing the outline
+            return jsonify({"image_data": image_data, "contours": contour_dict})
 
-    cv2.imshow('Image with Contours', img)  # display
-    cv2.waitKey(0)  # wait until key press
-    cv2.destroyAllWindows()
-else:
-    print("No contours found.")
+        else:
+            return jsonify({"error": "No contours found."})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
