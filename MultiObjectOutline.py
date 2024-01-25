ff9348d14ca2bef8e8102858d359e3eb56a815b1
bishal <bishalbhaandari62@gmail.com>
2024-01-25 10:35:20 +0100

    -Even small object can be identified with addition of threshold

--- MultiObjectOutline.py
+++ MultiObjectOutline.py
@@ -18,8 +18,8 @@ def detect_contours():
         image = cv2.imdecode(np_ary, cv2.IMREAD_COLOR)  # to color readable form
         bnw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gus_blr = cv2.GaussianBlur(bnw, (5, 5), 0)
-        edges = cv2.Canny(gus_blr, 50, 150)
-        contour_img, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        _, roi = cv2.threshold(gus_blr, 0, 255, cv2.THRESH_BINARY)  # image thresholding for segmenting
+        contour_img, _ = cv2.findContours(roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if contour_img:
             all_contour_points = [contour.reshape(-1, 2).tolist() for contour in contour_img]
             contour_dict = {i + 1: sublist for i, sublist in enumerate(all_contour_points)}
